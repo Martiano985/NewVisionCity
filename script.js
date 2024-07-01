3216406db3de92c4064a3501ecd1cc3b26bb6f58
Martiano985 <161725731+Martiano985@users.noreply.github.com>
2024-07-01 07:19:36 -0700

    project setup

---
+++ script.js
@@ -0,0 +1,38 @@
+let navBar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navBar.classList.toggle('active');
+   searchForm.classList.remove('active');
+   cartItem.classList.remove('active');
+
+}
+
+let cartItem = document.querySelector('.cart-item-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+   cartItem.classList.toggle('active');
+   navBar.classList.remove('active');
+   searchForm.classList.remove('active');
+
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+   searchForm.classList.toggle('active');
+   navBar.classList.remove('active');
+   cartItem.classList.remove('active');
+
+}
+
+window.onscroll = () =>{
+   navBar.classList.remove('active');
+   searchForm.classList.remove('active');
+   cartItem.classList.remove('active');
+
+
+}
+
+
+
+
